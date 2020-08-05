81389ecd61dd23fa2b2d6e2858e9234ac39e8cdd
Sergio <sergio3996@gmail.com>
2020-08-05 15:48:53 -0300

    products
    Agregado el listado de productos

--- js/products.js
+++ js/products.js
@@ -1,6 +1,42 @@
+var productsArray = [];
+
+function showProductsList(array) {
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < array.length; i++) {
+        let product = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                    <div class="mb-1">
+                        <h4>`+ product.name + `</h4>
+                        <p>`+ product.description + `</p>
+                    </div>
+                        <small class="text-muted">Precio: ` + product.currency + product.cost + `</small>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("productslist").innerHTML = htmlContentToAppend;
+    }
+}
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productsArray = resultObj.data;
+            showProductsList(productsArray);
+        }
+    });
 });
