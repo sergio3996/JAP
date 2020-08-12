d04235df2ab86c7148a94067bf773c538f15ddb1
Sergio <sergio3996@gmail.com>
2020-08-12 12:35:56 -0300

    login.js

--- js/login.js
+++ js/login.js
@@ -2,5 +2,20 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("ingresar").addEventListener("click", function (e){
+        let password = document.getElementById("Inpassword");
+        let email = document.getElementById("Inemail");
+        let camposCompletos = true;
+
+        if (password.value === '' || email.value === ''){
+            camposCompletos = false;
+            alert("Debes ingresar los datos!");
+        }
+        if (camposCompletos){
+            window.location = 'index.html';
+        }
+       
+
+    });
 
 });
