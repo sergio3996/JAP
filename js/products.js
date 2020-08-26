33d5899200da4fab8ee48ca1c89d8a2d1561491e
Sergio <sergio3996@gmail.com>
2020-08-26 13:38:43 -0300

    products.js
    Barra de busqueda

--- js/products.js
+++ js/products.js
@@ -5,29 +5,29 @@ var currentProductsArray = [];
 var currentSortCriteria = undefined;
 var minPrice = undefined;
 var maxPrice = undefined;
+var buscar = undefined;
 
-function sortProducts(criteria, array){
+function sortProducts(criteria, array) {
     let result = [];
-    if (criteria === ORDER_ASC_BY_PRICE)
-    {
-        result = array.sort(function(a, b) {
-            if ( a.cost < b.cost ){ return -1; }
-            if ( a.cost > b.cost ){ return 1; }
+    if (criteria === ORDER_ASC_BY_PRICE) {
+        result = array.sort(function (a, b) {
+            if (a.cost < b.cost) { return -1; }
+            if (a.cost > b.cost) { return 1; }
             return 0;
         });
-    }else if (criteria === ORDER_DESC_BY_PRICE){
-        result = array.sort(function(a, b) {
-            if ( a.cost > b.cost ){ return -1; }
-            if ( a.cost < b.cost ){ return 1; }
+    } else if (criteria === ORDER_DESC_BY_PRICE) {
+        result = array.sort(function (a, b) {
+            if (a.cost > b.cost) { return -1; }
+            if (a.cost < b.cost) { return 1; }
             return 0;
         });
-    }else if (criteria === ORDER_BY_PROD_SOLDCOUNT){
-        result = array.sort(function(a, b) {
+    } else if (criteria === ORDER_BY_PROD_SOLDCOUNT) {
+        result = array.sort(function (a, b) {
             let aSoldCount = parseInt(a.soldCount);
             let bSoldCount = parseInt(b.soldCount);
 
-            if ( aSoldCount > bSoldCount ){ return -1; }
-            if ( aSoldCount < bSoldCount ){ return 1; }
+            if (aSoldCount > bSoldCount) { return -1; }
+            if (aSoldCount < bSoldCount) { return 1; }
             return 0;
         });
     }
@@ -44,9 +44,10 @@ function showProductsList() {
         let product = currentProductsArray[i];
 
         if (((minPrice == undefined) || (minPrice != undefined && parseInt(product.cost) >= minPrice)) &&
-            ((maxPrice == undefined) || (maxPrice != undefined && parseInt(product.cost) <= maxPrice))){
+            ((maxPrice == undefined) || (maxPrice != undefined && parseInt(product.cost) <= maxPrice))) {
 
-        htmlContentToAppend += `
+            if (buscar == undefined || product.name.toLowerCase().indexOf(buscar) != -1) {
+                htmlContentToAppend += `
         <div class="list-group-item list-group-item-action">
             <div class="row">
                 <div class="col-3">
@@ -61,28 +62,29 @@ function showProductsList() {
                         <small class="text-muted">Precio: ` + product.currency + product.cost + `</small>
                         
                     </div>
-                    <p>`+ product.soldCount +` Productos vendidos</p>
+                    <p>`+ product.soldCount + ` Productos vendidos</p>
                     
                 </div>
             </div>
         </div>
         `
+            }
         }
 
         document.getElementById("productslist").innerHTML = htmlContentToAppend;
     }
 }
 
-function sortAndShowProducts(sortCriteria, productsArray){
+function sortAndShowProducts(sortCriteria, productsArray) {
     currentSortCriteria = sortCriteria;
 
-    if(productsArray != undefined){
+    if (productsArray != undefined) {
         currentProductsArray = productsArray;
     }
 
     currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
 
-   
+
     showProductsList();
 }
 
@@ -90,24 +92,24 @@ function sortAndShowProducts(sortCriteria, productsArray){
 
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCTS_URL).then(function (resultObj) {
-        if (resultObj.status === "ok"){
+        if (resultObj.status === "ok") {
             sortAndShowProducts(ORDER_ASC_BY_PRICE, resultObj.data);
         }
     });
 
-    document.getElementById("sortAsc").addEventListener("click", function(){
+    document.getElementById("sortAsc").addEventListener("click", function () {
         sortAndShowProducts(ORDER_ASC_BY_PRICE);
     });
 
-    document.getElementById("sortDesc").addEventListener("click", function(){
+    document.getElementById("sortDesc").addEventListener("click", function () {
         sortAndShowProducts(ORDER_DESC_BY_PRICE);
     });
 
-    document.getElementById("sortByRelev").addEventListener("click", function(){
+    document.getElementById("sortByRelev").addEventListener("click", function () {
         sortAndShowProducts(ORDER_BY_PROD_SOLDCOUNT);
     });
 
-    document.getElementById("clearRangePrice").addEventListener("click", function(){
+    document.getElementById("clearRangePrice").addEventListener("click", function () {
         document.getElementById("rangeFilterPriceMin").value = "";
         document.getElementById("rangeFilterPriceMax").value = "";
 
@@ -117,27 +119,32 @@ document.addEventListener("DOMContentLoaded", function (e) {
         showProductsList();
     });
 
-    document.getElementById("rangeFilterPrice").addEventListener("click", function(){
+    document.getElementById("rangeFilterPrice").addEventListener("click", function () {
         minPrice = document.getElementById("rangeFilterPriceMin").value;
         maxPrice = document.getElementById("rangeFilterPriceMax").value;
 
-        if ((minPrice != undefined) && (minPrice != "") && (parseInt(minPrice)) >= 0){
+        if ((minPrice != undefined) && (minPrice != "") && (parseInt(minPrice)) >= 0) {
             minPrice = parseInt(minPrice);
         }
-        else{
+        else {
             minPrice = undefined;
         }
 
-        if ((maxPrice != undefined) && (maxPrice != "") && (parseInt(maxPrice)) >= 0){
+        if ((maxPrice != undefined) && (maxPrice != "") && (parseInt(maxPrice)) >= 0) {
             maxPrice = parseInt(maxPrice);
         }
-        else{
+        else {
             maxPrice = undefined;
         }
 
         showProductsList();
-    
+
     });
+    
+    document.getElementById('searchbar').addEventListener('input', function(){
+        buscar = document.getElementById('searchbar').value.toLowerCase();
+        showProductsList();
+    })
 
 
 
